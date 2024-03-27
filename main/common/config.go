1d038a6f148195768c0b9d47df8a3d7924199fcd
Alessia Cinelli <cinellialessia@gmail.com>
2024-03-27 15:50:48 +0100

    Update

--- main/common/config.go
+++ main/common/config.go
@@ -1,4 +1,6 @@
-package main
+package common
+
+import "github.com/google/uuid"
 
 // Configurazione globale del programma
 
@@ -7,3 +9,9 @@ const Replicas = 3
 
 // ReplicaPorts Lista delle porte su cui le repliche possono essere contattate
 var ReplicaPorts = [Replicas]string{"8085", "8086", "8087"}
+
+// GenerateUniqueID Genera un ID univoco utilizzando UUID
+func GenerateUniqueID() string {
+	id := uuid.New()
+	return id.String()
+}
