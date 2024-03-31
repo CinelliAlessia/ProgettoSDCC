d5dc921d195bef3897c412973e224b2d3095a34d
Alessia Cinelli <cinellialessia@gmail.com>
2024-03-31 21:17:25 +0200

    Assegnato ordinamento deterministico a parità di timestamp in addToSortQueue

--- main/server/MulticastTotalOrdered.go
+++ main/server/MulticastTotalOrdered.go
@@ -107,27 +107,32 @@ func (kvs *KeyValueStoreSequential) ReceiveAck(message Message, reply *bool) err
 	return nil
 }
 
+// addToSortQueue aggiunge un messaggio alla coda locale, ordinandola in base al timeStamp, a parità di timestamp
+// l'ordinamento è deterministico, per garantire ordinamento totale, ordinandolo in base all'id.
 func (kvs *KeyValueStoreSequential) addToSortQueue(message Message) {
 	kvs.mu.Lock()
 	kvs.queue = append(kvs.queue, message)
 
 	// Ordina la coda in base al logicalClock
 	sort.Slice(kvs.queue, func(i, j int) bool {
+		if kvs.queue[i].LogicalClock == kvs.queue[j].LogicalClock {
+			return kvs.queue[i].Id < kvs.queue[j].Id
+		}
 		return kvs.queue[i].LogicalClock < kvs.queue[j].LogicalClock
 	})
+
 	kvs.mu.Unlock()
 }
 
-// controlSendToApplication
+// controlSendToApplication verifica se è possibile inviare la richiesta a livello apllicativo
+// 4. pj consegna msg_i all’applicazione se msg_i è in testa a queue_j, tutti gli ack relativi a msg_i sono stati ricevuti
+// da pj e, per ogni processo pk, c’è un messaggio msg_k in queue_j con timestamp maggiore di quello di msg_i
+// (quest’ultima condizione sta a indicare che nessun altro processo può inviare in multicast un messaggio con
+// timestamp potenzialmente minore o uguale a quello di msg_i)
 func (kvs *KeyValueStoreSequential) controlSendToApplication(message Message) bool {
-	//4. pj consegna msg_i all’applicazione se msg_i è in testa a queue_j, tutti gli ack relativi a msg_i sono stati ricevuti
-	//da pj e, per ogni processo pk, c’è un messaggio msg_k in queue_j con timestamp maggiore di quello di msg_i
-	//(quest’ultima condizione sta a indicare che nessun altro processo può inviare in multicast un messaggio con
-	//timestamp potenzialmente minore o uguale a quello di msg_i).
-	fmt.Println("controlSendToApplication: Controllo")
+
 	if kvs.queue[0].Id == message.Id && kvs.queue[0].NumberAck == common.Replicas /*&& kvs.queue[0].LogicalClock == message.LogicalClock*/ {
-		// Invia il messaggio all'applicazione
-		fmt.Println("controlSendToApplication: Ho ricevuto tutti gli ack, posso eliminare il messaggio dalla mia coda")
+		//fmt.Println("controlSendToApplication: Ho ricevuto tutti gli ack, posso eliminare il messaggio dalla mia coda")
 		kvs.removeMessage(message)
 		return true
 	} else {
