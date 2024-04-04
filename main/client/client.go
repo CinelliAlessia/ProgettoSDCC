940acd7ee21c9ab643c1f909bfae509d019feabf
Alessia Cinelli <cinellialessia@gmail.com>
2024-04-04 09:22:59 +0200

    Update

--- main/client/client.go
+++ main/client/client.go
@@ -6,6 +6,7 @@ import (
 	"main/common"
 	"math/rand"
 	"net/rpc"
+	"time"
 )
 
 func main() {
@@ -106,7 +107,7 @@ func causal() {
 	}
 	fmt.Println("CLIENT: Richiesta put effettuata " + reply.Reply)
 
-	//time.Sleep(time.Millisecond * 1000)
+	time.Sleep(time.Millisecond * 1000)
 	//fmt.Print("\nContinuare con la get: ")
 	//var choice int
 	//_, err = fmt.Scan(&choice)
