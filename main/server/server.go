a7e33bc41fda37664c8bd5b527833ed1cd8147ea
Alessia Cinelli <cinellialessia@gmail.com>
2024-04-04 09:26:16 +0200

    Update Argomenti passati come parametro della chiamata, da modificare

--- main/server/server.go
+++ main/server/server.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net"
 	"net/rpc"
+	"os"
+	"strconv"
 )
 
 // server.go
@@ -12,6 +14,25 @@ import (
 // e rispondono in maniera adeguata.
 
 func main() {
+
+	// Legge l'argomento passato
+	if len(os.Args) < 3 {
+		fmt.Println("Usare: ", os.Args[0], "<Porta di ascolto>", "<ID_Server>")
+		os.Exit(1)
+	}
+
+	port := os.Args[1]
+
+	// Legge l'ID del server passato come argomento dalla riga di comando
+	idStr := os.Args[2]
+
+	// Converti l'ID del server in un intero
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		fmt.Println("Errore:", err)
+		os.Exit(1)
+	}
+
 	// Inizializzazione delle strutture KeyValueStoreCausale e KeyValueStoreSequential
 
 	// ----- CONSISTENZA CAUSALE -----
@@ -19,6 +40,7 @@ func main() {
 		datastore:   make(map[string]string),
 		vectorClock: make([]int, 0), // Inizializzazione dell'orologio vettoriale
 		queue:       make([]MessageC, 0),
+		id:          id,
 	}
 
 	// ----- CONSISTENZA SEQUENZIALE -----
@@ -29,7 +51,7 @@ func main() {
 	}
 
 	// Registrazione dei servizi RPC
-	err := rpc.RegisterName("KeyValueStoreCausale", kvCausale)
+	err = rpc.RegisterName("KeyValueStoreCausale", kvCausale)
 	if err != nil {
 		fmt.Println("SERVER: Errore durante la registrazione di KeyValueStoreCausale", err)
 		return
@@ -41,20 +63,22 @@ func main() {
 	}
 
 	// Ottieni la porta da una variabile d'ambiente o assegna un valore predefinito
-	/* port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080" // Porta predefinita se RPC_PORT non è impostata !!! lettura file di config?
-	} */
+	/*
+		port := os.Getenv("PORT")
+		if port == "" {
+			port = "8080" // Porta predefinita se RPC_PORT non è impostata !!! lettura file di config?
+		}
 
-	fmt.Println("Inserisci la porta:")
-	var port string
-	_, err = fmt.Scanln(&port)
-	if err != nil {
-		fmt.Println("SERVER: Errore durante la lettura dell'input:", err)
-		return
-	}
+		fmt.Println("Inserisci la porta:")
+		var port string
+		_, err = fmt.Scanln(&port)
+		if err != nil {
+			fmt.Println("SERVER: Errore durante la lettura dell'input:", err)
+			return
+	}*/
 
 	// Avvio del listener RPC sulla porta specificata
+	fmt.Println("LA MIA PORTA", port)
 	listener, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		fmt.Println("SERVER: Errore nell'avvio del listener RPC:", err)
