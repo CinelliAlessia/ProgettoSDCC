76535227b194a27fd8fef54b28d4335ef78b671a
Alessia Cinelli <cinellialessia@gmail.com>
2024-04-04 17:22:30 +0200

    Update start, utilizzare il file bash e modificare la variabile CONFIG per la scelta

--- main/start.sh
+++ main/start.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-# Leggi il numero di repliche dalla variabile d'ambiente
-REPLICATION_SERVER="$REPLICATION_SERVER"
+# Imposta la variabile di ambiente
+export CONFIG=1 # 1 locale, 2 docker
 
-# Sostituisci il segnaposto nel file docker-compose.yml con il numero di repliche
-sed -i "s/REPLICA_PLACEHOLDER/$REPLICATION_SERVER/" docker-compose.yml
+if [ "$CONFIG" -eq 1 ]; then
+  # Percorsi dei file Go
+  server_file="server/"
+  client_file="client/"
+
+  # Esegui il server in un nuovo terminale gnome-terminal
+  gnome-terminal -- bash -c "cd $server_file && go run . 0; exec bash" &
+  gnome-terminal -- bash -c "cd $server_file && go run . 1; exec bash" &
+  gnome-terminal -- bash -c "cd $server_file && go run . 2; exec bash" &
+
+  # Esegui il client in un nuovo terminale gnome-terminal
+  gnome-terminal -- bash -c "cd $client_file && go run .; exec bash" &
+elif [ "$CONFIG" -eq 2 ]; then
+  # Esegui Docker Compose
+  docker-compose up --build
+else
+  echo "Configurazione non supportata."
+fi
