0347d3f0ac47611e4615dbcb67b1b3c80b8f9eef
Alessia Cinelli <cinellialessia@gmail.com>
2024-04-04 09:22:17 +0200

    Implementazione Multicast Causalmente Ordinato

--- main/server/CausallyOrderedMulticast.go
+++ main/server/CausallyOrderedMulticast.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"main/common"
+	"reflect"
+	"sort"
+	"time"
+)
+
 /* Multicast causalmente ordinato utilizzano il clock logico vettoriale.
 
 1. Quando il processo `pi` invia un messaggio `m`, crea un timestamp `t(m)` basato sul clock logico vettoriale `Vi`,
@@ -25,6 +33,96 @@ Quando il processo P_j riceve un messaggio multicast M da P_i:
 Questa implementazione garantisce che i messaggi vengano consegnati solo quando sono stati rispettati i vincoli causali
 	definiti nel tuo scenario.*/
 
-func (kvc *KeyValueStoreCausale) CausallyOrderedMulticast() {
+func (kvc *KeyValueStoreCausale) CausallyOrderedMulticast(message MessageC, reply *bool) error {
+	fmt.Println("CausallyOrderedMulticast: Ho ricevuto la richiesta che mi è stata inoltrata da un server")
 
+	kvc.addToSortQueue(message)
+
+	// Aggiornamento del clock
+	kvc.mutexClock.Lock()
+	for i := range len(kvc.vectorClock) {
+		kvc.vectorClock[i] = common.Max(message.VectorClock[i], kvc.vectorClock[i])
+	}
+	kvc.mutexClock.Unlock()
+
+	// Ciclo finché controlSendToApplication non restituisce true
+	for {
+		canSend := kvc.controlSendToApplication(message)
+		if canSend {
+
+			// Invio a livello applicativo
+			var replySaveInDatastore common.Response
+			err := kvc.RealFunction(message, &replySaveInDatastore)
+			if err != nil {
+				return err
+			}
+
+			*reply = true
+			break // Esci dal ciclo se controlSendToApplication restituisce true
+		}
+
+		// Altrimenti, attendi un breve periodo prima di riprovare
+		time.Sleep(time.Millisecond * 100)
+	}
+	return nil
+}
+
+func (kvc *KeyValueStoreCausale) addToSortQueue(message MessageC) {
+	kvc.mutexQueue.Lock()
+	defer kvc.mutexQueue.Unlock()
+
+	kvc.queue = append(kvc.queue, message)
+
+	// Ordina la coda in base all'orologio vettoriale e altri criteri
+	sort.Slice(kvc.queue, func(i, j int) bool {
+		// Confronto basato sull'orologio vettoriale
+		for idx := range kvc.queue[i].VectorClock {
+			if kvc.queue[i].VectorClock[idx] != kvc.queue[j].VectorClock[idx] {
+				return kvc.queue[i].VectorClock[idx] < kvc.queue[j].VectorClock[idx]
+			}
+		}
+		// Se l'orologio vettoriale è lo stesso, ordina in base all'ID
+		return kvc.queue[i].Id < kvc.queue[j].Id
+	})
+}
+
+/*
+2. Quando il processo `pj` riceve il messaggio `m` da `pi`, lo mette in una coda d'attesa e ritarda la consegna al
+livello applicativo finché non si verificano entrambe le seguenti condizioni:
+  - `t(m)[i] = Vj[i] + 1` (il messaggio `m` è il successivo che `pj` si aspetta da `pi`).
+  - `t(m)[k] ≤ Vj[k]` per ogni processo `pk` diverso da `i` (ovvero `pj` ha visto almeno gli stessi messaggi di `pk` visti da `pi`).
+*/
+func (kvc *KeyValueStoreCausale) controlSendToApplication(message MessageC) bool {
+	// Verifica se il messaggio è il prossimo atteso
+	expectedTimestamp := make([]int, len(kvc.vectorClock))
+	copy(expectedTimestamp, kvc.vectorClock)
+
+	senderID := 0                 // Dovrai definire un modo per identificare l'ID del mittente
+	expectedTimestamp[senderID]++ // Il messaggio successivo che pj si aspetta da pi
+
+	if !reflect.DeepEqual(expectedTimestamp, message.VectorClock) {
+		return false
+	}
+
+	// Verifica se il messaggio ha ricevuto tutti gli ack
+	if message.NumberAck == common.Replicas {
+		// Il messaggio ha ricevuto tutti gli ack, quindi può essere rimosso dalla coda
+		kvc.removeMessage(message)
+		return true
+	}
+
+	return false
+}
+
+// removeByID Rimuove un messaggio dalla coda basato sull'ID del messaggio
+func (kvc *KeyValueStoreCausale) removeMessage(message MessageC) {
+	for i := range kvc.queue {
+		if kvc.queue[i].Id == message.Id {
+			// Rimuovi l'elemento dalla slice
+			kvc.queue = append(kvc.queue[:i], kvc.queue[i+1:]...)
+			fmt.Println("removeByID: Messaggio con ID", message.Id, "rimosso dalla coda")
+			return
+		}
+	}
+	fmt.Println("removeByID: Messaggio con ID", message.Id, "non trovato nella coda")
 }
--- main/server/KeyValueStoreCausale.go
+++ main/server/KeyValueStoreCausale.go
@@ -1,28 +1,159 @@
 package main
 
 import (
+	"fmt"
 	"main/common"
+	"net/rpc"
 	"sync"
 )
 
 // KeyValueStoreCausale rappresenta il servizio di memorizzazione chiave-valore
 type KeyValueStoreCausale struct {
-	data        map[string]string // Mappa -> struttura dati che associa chiavi a valori
-	vectorClock []int             // Orologio vettoriale
-	mutex       sync.Mutex
+	datastore map[string]string // Mappa -> struttura dati che associa chiavi a valori
+	id        int
+
+	vectorClock []int // Orologio vettoriale
+	mutexClock  sync.Mutex
+
+	queue      []MessageC
+	mutexQueue sync.Mutex // Mutex per proteggere l'accesso concorrente alla coda
+}
+
+type MessageC struct {
+	Id            string
+	TypeOfMessage string
+	Args          common.Args
+	VectorClock   []int // Orologio vettoriale
+	NumberAck     int
 }
 
 // Get restituisce il valore associato alla chiave specificata -> Lettura -> Evento interno
-func (kvs *KeyValueStoreCausale) Get(args common.Args, response *common.Response) error {
+func (kvc *KeyValueStoreCausale) Get(args common.Args, response *common.Response) error {
+	fmt.Println("Richiesta GET")
+
+	kvc.mutexClock.Lock()
+	kvc.vectorClock[kvc.id]++
+	kvc.mutexClock.Unlock()
+
+	id := common.GenerateUniqueID()
+	id = "b"
+
+	message := MessageC{id, "Get", args, kvc.vectorClock, 0}
+	kvc.addToSortQueue(message)
+
+	for {
+		if kvc.queue[0].Id == message.Id /*&& sort come si deve*/ {
+			kvc.mutexClock.Lock()
+			val, ok := kvc.datastore[message.Args.Key]
+			if !ok {
+				fmt.Println("Key non trovata nel datastore", message.Args.Key)
+				fmt.Println(kvc.datastore)
+				return fmt.Errorf("KeyValueStoreCausale: key '%s' not found", id)
+			}
+			kvc.removeMessage(message)
+			kvc.mutexClock.Unlock()
+			response.Reply = val
+			break
+		}
+	}
+	fmt.Println("DATASTORE:")
+	fmt.Println(kvc.datastore)
 	return nil
 }
 
 // Put inserisce una nuova coppia chiave-valore, se la chiave è già presente, sovrascrive il valore associato
-func (kvs *KeyValueStoreCausale) Put(args common.Args, response *common.Response) error {
+func (kvc *KeyValueStoreCausale) Put(args common.Args, response *common.Response) error {
+	fmt.Println("KeyValueStoreCausale: Comando PUT eseguito")
+
+	kvc.mutexClock.Lock()
+	kvc.vectorClock[kvc.id]++
+	kvc.mutexClock.Unlock()
+
+	id := common.GenerateUniqueID()
+	id = "a"
+
+	// CREO IL MESSAGGIO E DEVO FAR SI CHE TUTTI LO SCRIVONO NEL DATASTORE
+	message := MessageC{id, "Put", args, kvc.vectorClock, 0}
+	var reply *bool
+	err := kvc.sendToOtherServer("KeyValueStoreCausale.CausallyOrderedMulticast", message, reply)
+	if err != nil {
+		return err
+	}
+
+	response.Reply = "true"
 	return nil
 }
 
 // Delete elimina una coppia chiave-valore, è un operazione di scrittura
-func (kvs *KeyValueStoreCausale) Delete(args common.Args, response *common.Response) error {
+func (kvc *KeyValueStoreCausale) Delete(args common.Args, response *common.Response) error {
+	fmt.Println("KeyValueStoreCausale: Comando PUT eseguito")
+
+	kvc.mutexClock.Lock()
+	kvc.vectorClock[kvc.id]++
+	kvc.mutexClock.Unlock()
+
+	id := common.GenerateUniqueID()
+	id = "a"
+
+	// CREO IL MESSAGGIO E DEVO FAR SI CHE TUTTI LO SCRIVONO NEL DATASTORE
+	message := MessageC{id, "Delete", args, kvc.vectorClock, 0}
+	var reply *bool
+	err := kvc.sendToOtherServer("KeyValueStoreCausale.CausallyOrderedMulticast", message, reply)
+	if err != nil {
+		return err
+	}
+
+	response.Reply = "true"
+	return nil
+}
+
+// sendToOtherServer invia a tutti i server la richiesta rpcName
+func (kvc *KeyValueStoreCausale) sendToOtherServer(rpcName string, message MessageC, response *bool) error {
+
+	//var responseValues [common.Replicas]common.Response
+	for i := 0; i < common.Replicas; i++ {
+		go func(replicaPort string) {
+
+			conn, err := rpc.Dial("tcp", ":"+replicaPort)
+			if err != nil {
+				fmt.Printf("KeyValueStoreCausale: Errore durante la connessione al server "+replicaPort+": ", err)
+				return
+			}
+
+			// Chiama il metodo "rpcName" sul server
+			err = conn.Call(rpcName, message, &response)
+			if err != nil {
+				fmt.Println("KeyValueStoreCausale: Errore durante la chiamata RPC "+rpcName+": ", err)
+				return
+			}
+
+		}(common.ReplicaPorts[i])
+	}
+	return nil
+}
+
+// RealFunction esegue l'operazione di put e di delete realmente
+func (kvc *KeyValueStoreCausale) RealFunction(message MessageC, _ *common.Response) error {
+
+	if message.TypeOfMessage == "Put" { // Scrittura
+		kvc.mutexClock.Lock()
+		fmt.Println("Key inserita ", message.Args.Key)
+		kvc.datastore[message.Args.Key] = message.Args.Value
+		fmt.Println("DATASTORE:")
+		fmt.Println(kvc.datastore)
+		kvc.mutexClock.Unlock()
+
+	} else if message.TypeOfMessage == "Delete" { // Scrittura
+		kvc.mutexClock.Lock()
+		fmt.Println("Key eliminata ", message.Args.Key)
+		delete(kvc.datastore, message.Args.Key)
+		fmt.Println("DATASTORE:")
+		fmt.Println(kvc.datastore)
+		kvc.mutexClock.Unlock()
+
+	} else {
+		return fmt.Errorf("command not found")
+	}
+
 	return nil
 }
