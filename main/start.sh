f4ae95e2aac35e414d0c00e4dd8e7b092d613119
Alessia Cinelli <cinellialessia@gmail.com>
2024-04-05 15:52:43 +0200

    Update

--- main/start.sh
+++ main/start.sh
@@ -4,17 +4,19 @@
 export CONFIG=1 # 1 locale, 2 docker
 
 if [ "$CONFIG" -eq 1 ]; then
+
   # Percorsi dei file Go
   server_file="server/"
   client_file="client/"
 
   # Esecuzione del server in un nuovo terminale gnome-terminal
-  gnome-terminal -- bash -c "cd $server_file && go run . 0; exec bash" &
-  gnome-terminal -- bash -c "cd $server_file && go run . 1; exec bash" &
-  gnome-terminal -- bash -c "cd $server_file && go run . 2; exec bash" &
+  gnome-terminal --geometry=100x24+0+0 -- bash -c "cd server/ && go run . 0; exec bash" &
+  gnome-terminal --geometry=100x24+0+0 -- bash -c "cd $server_file && go run . 1; exec bash" &
+  gnome-terminal --geometry=100x24+0+0 -- bash -c "cd $server_file && go run . 2; exec bash" &
 
   # Esecuzione del client in un nuovo terminale gnome-terminal
-  gnome-terminal -- bash -c "cd $client_file && go run .; exec bash" &
+  gnome-terminal --geometry=100x24+0+0 -- bash -c "cd $client_file && go run .; exec bash" &
+
 elif [ "$CONFIG" -eq 2 ]; then
   # Esecuzione con Docker Compose
   docker-compose up --build
