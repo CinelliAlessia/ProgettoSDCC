9a38b87c1697e67e90d451d0fc1161c7e7ce9974
Alessia Cinelli <cinellialessia@gmail.com>
2024-04-05 16:21:51 +0200

    Correzione associazione clock al messaggio Modifica gestione evento interno

--- main/server/KeyValueStoreSequential.go
+++ main/server/KeyValueStoreSequential.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"strconv"
 	"sync"
+	"time"
 )
 
 // KeyValueStoreSequential rappresenta il servizio di memorizzazione chiave-valore specializzato nel sequenziale
@@ -33,34 +34,53 @@ type Message struct {
 func (kvs *KeyValueStoreSequential) Get(args common.Args, response *common.Response) error {
 	fmt.Println("Richiesta GET di", args.Key)
 
+	id := common.GenerateUniqueID() //Id univoco da assegnare al messaggio
+
+	// Incrementa il clock logico e genera il messaggio da inviare a livello applicativo
 	kvs.mutexClock.Lock()
 	kvs.logicalClock++
+	message := Message{id, "Get", args, kvs.logicalClock, 3}
 	kvs.mutexClock.Unlock()
 
-	id := common.GenerateUniqueID()
-
-	message := Message{id, "Get", args, kvs.logicalClock, 0}
-	kvs.addToSortQueue(message)
-
+	kvs.addToSortQueue(message) //Aggiunge alla coda ordinandolo per timestamp, cosi verrà letto esclusivamente se
 	for {
-		if kvs.queue[0].Id == message.Id && kvs.queue[0].LogicalClock == message.LogicalClock {
-			// la seconda condizione dell'if credo sia inutile, se l'id è davvero univoco
-			// proprio perché è un id associato al messaggio e non una key
-			val, ok := kvs.datastore[message.Args.Key]
-			if !ok {
-				fmt.Println("Key non trovata nel datastore", message.Args.Key)
-				fmt.Println(kvs.datastore)
-				return fmt.Errorf("KeyValueStoreSequential: key '%s' not found", message.Args.Key)
+		canSend := kvs.controlSendToApplication(message)
+		if canSend {
+
+			// Invio a livello applicativo
+			err := kvs.RealFunction(message, response)
+			if err != nil {
+				return err
 			}
-			kvs.removeMessage(message)
-			response.Reply = val
+
 			break
 		}
+
+		// Altrimenti, attendi un breve periodo prima di riprovare
+		time.Sleep(time.Millisecond * 100)
 	}
+	/*
+		for {
+			if kvs.queue[0].Id == message.Id {
+				// la seconda condizione dell'if credo sia inutile, se l'id è davvero univoco
+				// proprio perché è un id associato al messaggio e non una key
+				val, ok := kvs.datastore[message.Args.Key]
+				if !ok {
+					fmt.Println("Key non trovata nel datastore", message.Args.Key)
+					fmt.Println(kvs.datastore)
+					return fmt.Errorf("KeyValueStoreSequential: key '%s' not found", message.Args.Key)
+				}
+				response.Reply = val
 
-	fmt.Println("Comando GET eseguito correttamente, DATASTORE:")
-	fmt.Println(kvs.datastore)
+				fmt.Println("CONTROLLO GET OK")
 
+				kvs.removeMessageToQueue(message)
+				break
+			} else {
+				fmt.Println("CONTROLLO GET", kvs.queue[0].Id, kvs.queue[0].Args.Key, kvs.queue[0].LogicalClock, "messaggio", message.Id, message.Args.Key, message.Args.Value, message.LogicalClock)
+			}
+			time.Sleep(time.Millisecond * 500)
+		}*/
 	return nil
 }
 
@@ -68,17 +88,14 @@ func (kvs *KeyValueStoreSequential) Get(args common.Args, response *common.Respo
 func (kvs *KeyValueStoreSequential) Put(args common.Args, response *common.Response) error {
 	fmt.Println("Richiesta PUT di", args.Key, args.Value)
 
+	// CREO IL MESSAGGIO E DEVO FAR SI CHE TUTTI LO SCRIVONO NEL DATASTORE
 	kvs.mutexClock.Lock()
 	kvs.logicalClock++
+	id := common.GenerateUniqueID()
+	message := Message{id, "Put", args, kvs.logicalClock, 0}
 	kvs.mutexClock.Unlock()
 
-	id := common.GenerateUniqueID()
-
-	// CREO IL MESSAGGIO E DEVO FAR SI CHE TUTTI LO SCRIVONO NEL DATASTORE
-	message := Message{id, "Put", args, kvs.logicalClock, 0}
-
-	var reply *bool
-	err := kvs.sendToOtherServer("KeyValueStoreSequential.MulticastTotalOrdered", message, reply)
+	err := kvs.sendToOtherServer("KeyValueStoreSequential.MulticastTotalOrdered", message)
 	if err != nil {
 		return err
 	}
@@ -92,13 +109,14 @@ func (kvs *KeyValueStoreSequential) Delete(args common.Args, response *common.Re
 
 	kvs.mutexClock.Lock()
 	kvs.logicalClock++
-	kvs.mutexClock.Unlock()
 
 	// CREO IL MESSAGGIO E DEVO FAR SI CHE TUTTI LO SCRIVONO NEL DATASTORE
-	message := Message{common.GenerateUniqueID(), "Delete", args, kvs.logicalClock, 0}
+	id := common.GenerateUniqueID()
+	message := Message{id, "Delete", args, kvs.logicalClock, 0}
 
-	var reply *bool
-	err := kvs.sendToOtherServer("KeyValueStoreSequential.MulticastTotalOrdered", message, reply)
+	kvs.mutexClock.Unlock()
+
+	err := kvs.sendToOtherServer("KeyValueStoreSequential.MulticastTotalOrdered", message)
 	if err != nil {
 		return err
 	}
@@ -108,24 +126,20 @@ func (kvs *KeyValueStoreSequential) Delete(args common.Args, response *common.Re
 }
 
 // RealFunction esegue l'operazione di put e di delete realmente
-func (kvs *KeyValueStoreSequential) RealFunction(message Message, _ *common.Response) error {
+func (kvs *KeyValueStoreSequential) RealFunction(message Message, response *common.Response) error {
 
 	if message.TypeOfMessage == "Put" { // Scrittura
-		kvs.mutexClock.Lock()
-		fmt.Println("Key inserita ", message.Args.Key)
 		kvs.datastore[message.Args.Key] = message.Args.Value
-		fmt.Println("DATASTORE:")
-		fmt.Println(kvs.datastore)
-		kvs.mutexClock.Unlock()
-
 	} else if message.TypeOfMessage == "Delete" { // Scrittura
-		kvs.mutexClock.Lock()
-		fmt.Println("Key eliminata ", message.Args.Key)
 		delete(kvs.datastore, message.Args.Key)
-		fmt.Println("DATASTORE:")
-		fmt.Println(kvs.datastore)
-		kvs.mutexClock.Unlock()
-
+	} else if message.TypeOfMessage == "Get" {
+		val, ok := kvs.datastore[message.Args.Key]
+		if !ok {
+			fmt.Println("Key non trovata nel datastore", message.Args.Key)
+			fmt.Println(kvs.datastore)
+			return fmt.Errorf("KeyValueStoreSequential: key '%s' not found", message.Args.Key)
+		}
+		response.Reply = val
 	} else {
 		return fmt.Errorf("command not found")
 	}
@@ -134,42 +148,58 @@ func (kvs *KeyValueStoreSequential) RealFunction(message Message, _ *common.Resp
 }
 
 // sendToOtherServer invia a tutti i server la richiesta rpcName
-func (kvs *KeyValueStoreSequential) sendToOtherServer(rpcName string, message Message, response *bool) error {
-	// NON CONTROLLO MAI IL VALORE DI RESPONSE; ESSENDO UNIVOCO PER TUTTE E TRE POTREI ANCHE LEGGERLO IN MANIERA ERRATA
+func (kvs *KeyValueStoreSequential) sendToOtherServer(rpcName string, message Message) error {
+	// Canale per ricevere i risultati delle chiamate RPC
+	resultChan := make(chan error, common.Replicas)
 
-	//var responseValues [common.Replicas]common.Response
+	// Itera su tutte le repliche e avvia le chiamate RPC
 	for i := 0; i < common.Replicas; i++ {
-		i := i
-		go func(replicaPort string) {
+		go kvs.callRPC(rpcName, message, resultChan, i)
+	}
 
-			var serverName string
-
-			if os.Getenv("CONFIG") == "1" {
-				/*---LOCALE---*/
-				serverName = ":" + replicaPort
-			} else if os.Getenv("CONFIG") == "2" {
-				/*---DOCKER---*/
-				serverName = "server" + strconv.Itoa(i+1) + ":" + replicaPort
-			} else {
-				fmt.Println("VARIABILE DI AMBIENTE ERRATA")
-				return
-			}
-
-			fmt.Println("sendToOtherServer: Contatto il server:", serverName)
-			conn, err := rpc.Dial("tcp", serverName)
-			if err != nil {
-				fmt.Printf("sendToOtherServer: Errore durante la connessione al server:"+replicaPort, err)
-				return
-			}
-
-			// Chiama il metodo "rpcName" sul server
-			err = conn.Call(rpcName, message, &response)
-			if err != nil {
-				fmt.Println("sendToOtherServer: Errore durante la chiamata RPC "+rpcName+": ", err)
-				return
-			}
-
-		}(common.ReplicaPorts[i])
+	// Raccoglie i risultati dalle chiamate RPC
+	for i := 0; i < common.Replicas; i++ {
+		if err := <-resultChan; err != nil {
+			// Gestisci l'errore in base alle tue esigenze
+			return err
+		}
 	}
 	return nil
+}
+
+// callRPC è una funzione ausiliaria per effettuare la chiamata RPC a una replica specifica
+func (kvs *KeyValueStoreSequential) callRPC(rpcName string, message Message, resultChan chan<- error, replicaIndex int) {
+	var serverName string
+
+	if os.Getenv("CONFIG") == "1" {
+		/*---LOCALE---*/
+		serverName = ":" + common.ReplicaPorts[replicaIndex]
+	} else if os.Getenv("CONFIG") == "2" {
+		/*---DOCKER---*/
+		serverName = "server" + strconv.Itoa(replicaIndex+1) + ":" + common.ReplicaPorts[replicaIndex]
+	} else {
+		// Gestione dell'errore se la variabile di ambiente non è corretta
+		resultChan <- fmt.Errorf("VARIABILE DI AMBIENTE ERRATA")
+		return
+	}
+
+	fmt.Println("sendToOtherServer: Contatto il server:", serverName)
+	conn, err := rpc.Dial("tcp", serverName)
+	if err != nil {
+		// Gestione dell'errore durante la connessione al server
+		resultChan <- fmt.Errorf("errore durante la connessione al server %s: %v", serverName, err)
+		return
+	}
+
+	// Chiama il metodo "rpcName" sul server
+	var response bool
+	err = conn.Call(rpcName, message, &response)
+	if err != nil {
+		// Gestione dell'errore durante la chiamata RPC
+		resultChan <- fmt.Errorf("errore durante la chiamata RPC %s a %s: %v", rpcName, serverName, err)
+		return
+	}
+
+	// Aggiungi il risultato al canale dei risultati
+	resultChan <- nil
 }
