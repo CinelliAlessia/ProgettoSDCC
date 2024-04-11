cbb99c7a5508aba5290f73c66e4304a694e5bc65
Alessia Cinelli <cinellialessia@gmail.com>
2024-04-11 15:21:15 +0200

    Commenti aggiornati.

--- main/common/config.go
+++ main/common/config.go
@@ -1,13 +1,14 @@
+// Package common fornisce funzioni e variabili globali utilizzate in tutto il sistema.
 package common
 
 import (
 	"fmt"
 	"github.com/google/uuid"
+	"math/rand"
 	"os"
 	"strconv"
+	"time"
 )
-
-// Configurazione globale del programma
 
 // Replicas Numero di repliche del server -> almeno 3
 const Replicas = 3
@@ -21,6 +22,7 @@ func GenerateUniqueID() string {
 	return id.String()
 }
 
+// Max TODO: Controllare perché la funzione max non funziona
 func Max(clock int, clock2 int) int {
 	if clock > clock2 {
 		return clock
@@ -29,7 +31,7 @@ func Max(clock int, clock2 int) int {
 	}
 }
 
-// GetServerName restituisce il nome del server in base alla configurazione
+// GetServerName restituisce il nome del server in base alla scelta di configurazione, tra locale e remota (docker)
 func GetServerName(replicaPort string, id int) string {
 	if os.Getenv("CONFIG") == "1" {
 		return ":" + replicaPort // Locale
@@ -39,4 +41,12 @@ func GetServerName(replicaPort string, id int) string {
 		fmt.Println("VARIABILE DI AMBIENTE ERRATA")
 		return ""
 	}
+}
+
+// RandomDelay Genera un numero casuale compreso tra 0 e 999 (max un secondo)
+func RandomDelay() {
+	delay := rand.Intn(1000)
+
+	// Introduce un ritardo casuale
+	time.Sleep(time.Millisecond * time.Duration(delay))
 }
