3c1e027205f20da990927745317b5a5e86b0fd5e
Alessia Cinelli <cinellialessia@gmail.com>
2024-04-11 15:18:11 +0200

    Commenti aggiornati, correzione print per debug e inserito un delay random per le chiamate del client

--- main/client/client.go
+++ main/client/client.go
@@ -6,7 +6,6 @@ import (
 	"main/common"
 	"math/rand"
 	"net/rpc"
-	"time"
 )
 
 const (
@@ -29,11 +28,10 @@ func main() {
 		_, err := fmt.Scan(&choice)
 		if err != nil {
 			fmt.Println("Client -> Errore durante la lettura dell'input:", err)
-			//break
+			break
 		}
 
 		var rpcName string
-		//choice := 2
 		switch choice {
 		case 1:
 			fmt.Println("Scelta di consistenza causale")
@@ -47,30 +45,29 @@ func main() {
 			fmt.Println("Scelta non valida. Riprova.")
 		}
 
+		// Esecuzione delle rpc
 		done := make(chan bool)
-		go run1(rpcName, done)
-		go run2(rpcName, done)
-		go run3(rpcName, done)
-		go run4(rpcName, done)
-		time.Sleep(time.Millisecond * 100)
 		go run1(rpcName, done)
 		go run2(rpcName, done)
 		go run3(rpcName, done)
 		go run4(rpcName, done)
 
 		// Attendi il completamento di tutte le goroutine
-		for i := 0; i < 8; i++ {
+		for i := 0; i < 4; i++ {
 			<-done
 		}
 	}
 }
 
+// randomConnect restituisce una connessione random con un server definito in common/config.go
 func randomConnect() *rpc.Client {
 	// Genera un numero casuale tra 0 e il numero di repliche - 1
 	randomIndex := rand.Intn(common.Replicas)
+
+	// Ottengo l'indirizzo a cui connettermi
 	serverName := common.GetServerName(common.ReplicaPorts[randomIndex], randomIndex)
 
-	fmt.Println("CLIENT: Contatto il server", serverName)
+	//fmt.Println("CLIENT: Contatto il server", serverName)
 	conn, err := rpc.Dial("tcp", serverName)
 
 	if err != nil {
@@ -80,13 +77,17 @@ func randomConnect() *rpc.Client {
 	return conn
 }
 
+// specificConnect restituisce una connessione specifica con un server definito in common/config.go, tramite index passato in argomento
 func specificConnect(index int) *rpc.Client {
 	if index >= common.Replicas {
+		_ = fmt.Errorf("index out of range")
 		return nil
 	}
+
+	// Ottengo l'indirizzo a cui connettermi
 	serverName := common.GetServerName(common.ReplicaPorts[index], index)
 
-	fmt.Println("CLIENT: Contatto il server:", serverName)
+	//fmt.Println("CLIENT: Contatto il server:", serverName)
 	conn, err := rpc.Dial("tcp", serverName)
 
 	if err != nil {
@@ -96,17 +97,8 @@ func specificConnect(index int) *rpc.Client {
 	return conn
 }
 
-func randomDelay() {
-	// Genera un numero casuale compreso tra 0 e 999 (max un secondo)
-	//TODO: time.newTimer?
-	delay := rand.Intn(1000)
-
-	// Introduce un ritardo casuale
-	time.Sleep(time.Millisecond * time.Duration(delay))
-}
-
+// executeCall esegue un comando ad un server random. Il comando da eseguire viene specificato tramite i parametri inseriti
 func executeCall(rpcName, key string, values ...string) {
-	randomDelay()
 	var value string
 	if len(values) > 0 {
 		value = values[0]
@@ -119,25 +111,29 @@ func executeCall(rpcName, key string, values ...string) {
 		fmt.Println("CLIENT: Errore durante la connessione")
 		return
 	}
-	err := call(conn, args, &reply, rpcName)
+
+	// TODO: Qui posso usare un id auto-incrementativo per un DEBUG accurato
+	fmt.Println("Run", rpcName, key+":"+value)
+	err := delayedCall(conn, args, &reply, rpcName)
 	if err != nil {
 		return
 	}
-	fmt.Println("Run", rpcName, key+":"+value, reply.Reply)
+
 }
 
-func call(conn *rpc.Client, args common.Args, reply *common.Response, rpcName string) error {
+// delayedCall esegue una chiamata RPC ritardata utilizzando il client RPC fornito.
+// Prima di effettuare la chiamata, applica un ritardo casuale per simulare condizioni reali di rete.
+func delayedCall(conn *rpc.Client, args common.Args, reply *common.Response, rpcName string) error {
+	// Applica un ritardo casuale
+	common.RandomDelay()
+
+	// Effettua la chiamata RPC
 	err := conn.Call(rpcName, args, reply)
 	if err != nil {
-		return fmt.Errorf("CLIENT: Errore durante la chiamata RPC")
+		return fmt.Errorf("CLIENT: Errore durante la chiamata RPC in client.call")
 	}
 	return nil
 }
-
-//time.Sleep(time.Millisecond * 1000)
-//fmt.Print("\nContinuare con la get: ")
-//var choice int
-//_, err = fmt.Scan(&choice)
 
 func run1(rpcName string, done chan bool) {
 
