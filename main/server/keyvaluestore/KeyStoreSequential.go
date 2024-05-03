81b5547e86ba7da756b313d89e2a739407ae919d
Alessia Cinelli <cinellialessia@gmail.com>
2024-05-03 16:37:30 +0200

    Aggiunta mappa client e metodi per accedervi

--- main/server/keyvaluestore/KeyStoreSequential.go
+++ main/server/keyvaluestore/KeyStoreSequential.go
@@ -1,6 +1,8 @@
 package keyvaluestore
 
 import (
+	"errors"
+	"fmt"
 	"main/common"
 	"main/server/message"
 	"sync"
@@ -13,15 +15,16 @@ type KeyValueStoreSequential struct {
 	LogicalClock int // Orologio logico scalare
 	mutexClock   sync.Mutex
 
-	Queue      []msg.MessageS
+	Queue      []commonMsg.MessageS
 	mutexQueue sync.Mutex // Mutex per proteggere l'accesso concorrente alla coda
 
 	executeFunctionMutex sync.Mutex // Mutex aggiunto per evitare scheduling che interrompano l'invio a livello applicativo del messaggio
 
 	ClientMaps map[string]*ClientMap // Mappa -> struttura dati che associa chiavi a valori
+	mutexMaps  sync.Mutex            // Mutex per proteggere l'accesso concorrente alla mappa
 }
 
-// ----- Consistenza Sequenziale ----- //
+// ----- CONSISTENZA SEQUENZIALE ----- //
 
 func NewKeyValueStoreSequential(idServer int) *KeyValueStoreSequential {
 	kvSequential := &KeyValueStoreSequential{
@@ -31,106 +34,115 @@ func NewKeyValueStoreSequential(idServer int) *KeyValueStoreSequential {
 	}
 
 	kvSequential.ClientMaps = make(map[string]*ClientMap)
-	kvSequential.SetLogicalClock(0)                // Inizializzazione dell'orologio logico scalare
-	kvSequential.SetQueue(make([]msg.MessageS, 0)) // Inizializzazione della coda
+	kvSequential.SetClock(0)                             // Inizializzazione dell'orologio logico scalare
+	kvSequential.SetQueue(make([]commonMsg.MessageS, 0)) // Inizializzazione della coda
 	kvSequential.SetIdServer(idServer)
 
 	return kvSequential
 }
 
-func (kvs *KeyValueStoreSequential) SetDatastore(key string, value string) {
+// ----- Operazioni Datastore ----- //
+
+func (kvs *KeyValueStoreSequential) PutInDatastore(key string, value string) {
 	kvs.Common.Datastore[key] = value
 }
 
-func (kvs *KeyValueStoreSequential) SetLogicalClock(logicalClock int) {
-	kvs.LogicalClock = logicalClock
+// SetDatastore imposta il datastore del server
+func (kvs *KeyValueStoreSequential) SetDatastore(datastore map[string]string) {
+	kvs.Common.Datastore = datastore
 }
 
-func (kvs *KeyValueStoreSequential) SetQueue(queue []msg.MessageS) {
-	kvs.Queue = queue
-}
-
-func (kvs *KeyValueStoreSequential) SetIdServer(id int) {
-	kvs.Common.Id = id
-}
-
-func (kvs *KeyValueStoreSequential) IncreaseRequestTsClient(args common.Args) {
-	kvs.ClientMaps[args.GetIdClient()].MutexRequest.Lock()
-	defer kvs.ClientMaps[args.GetIdClient()].MutexRequest.Unlock()
-	kvs.ClientMaps[args.GetIdClient()].RequestTs = args.TimestampClient
-}
-
-func (kvs *KeyValueStoreSequential) GetRequestTsClient(id string) int {
-	return kvs.ClientMaps[id].RequestTs
-}
-
-func (kvs *KeyValueStoreSequential) GetClock() int {
-	return kvs.LogicalClock
+func (kvs *KeyValueStoreSequential) GetFromDatastore(key string) (string, error) {
+	if value, ok := kvs.Common.Datastore[key]; ok {
+		return value, nil
+	}
+	return "", errors.New("chiave non presente nel datastore")
 }
 
 func (kvs *KeyValueStoreSequential) GetDatastore() map[string]string {
 	return kvs.Common.Datastore
 }
 
-func (kvs *KeyValueStoreSequential) GetQueue() []msg.MessageS {
-	return kvs.Queue
+// ----- Orologio Logico Scalare ----- //
+
+func (kvs *KeyValueStoreSequential) SetClock(logicalClock int) {
+	kvs.LogicalClock = logicalClock
 }
 
+// GetClock gestisce una chiamata RPC di un evento interno, genera un messaggio e gli allega il suo clock scalare.
+func (kvs *KeyValueStoreSequential) GetClock() int {
+	return kvs.LogicalClock
+}
+
+// ----- Coda ----- //
+
+func (kvs *KeyValueStoreSequential) SetQueue(queue []commonMsg.MessageS) {
+	kvs.Queue = queue
+}
+func (kvs *KeyValueStoreSequential) GetQueue() []commonMsg.MessageS {
+	return kvs.Queue
+}
+func (kvs *KeyValueStoreSequential) GetMsgToQueue(index int) *commonMsg.MessageS {
+	return &kvs.GetQueue()[index]
+}
+
+// ----- Id Server ----- //
+
+func (kvs *KeyValueStoreSequential) SetIdServer(id int) {
+	kvs.Common.Id = id
+}
 func (kvs *KeyValueStoreSequential) GetIdServer() int {
 	return kvs.Common.Id
 }
 
-/*
-type orderingFIFO struct {
-	receiveAssumeFIFO      int // Variabile per mantenere le richieste dal client in ordine FIFO
-	receiveAssumeFIFOMutex sync.Mutex
+// ----- Client Map ----- //
 
-	sendAssumeFIFO      [Common.Replicas]int // Variabile per mantenere le richieste dal client in ordine FIFO
-	sendAssumeFIFOMutex sync.Mutex
-}
-{ // Questo in kvs
-	receiveAssumeFIFO      int // Variabile per mantenere le richieste dal client in ordine FIFO
-	receiveAssumeFIFOMutex sync.Mutex
+func (kvs *KeyValueStoreSequential) NewClientMap(idClient string) {
+	kvs.mutexMaps.Lock()
+	defer kvs.mutexMaps.Unlock()
 
-	sendAssumeFIFO      int // Variabile per mantenere le richieste dal client in ordine FIFO //[Common.Replicas]
-	sendAssumeFIFOMutex sync.Mutex
+	kvs.ClientMaps[idClient] = &ClientMap{RequestTs: 0, ExecuteTs: 0}
 }
 
+func (kvs *KeyValueStoreSequential) GetClientMap(id string) (*ClientMap, bool) {
+	val, ok := kvs.ClientMaps[id]
+	return val, ok
+}
 
-// receiveFIFOOrdered è una funzione di supporto utilizzata per mantenere l'illusione di una comunicazione FIFO
-// Le richieste inviate dal client hanno un timestamp scalare crescente, ma non è garantito che le richieste
-// arriveranno in ordine crescente, receiveFIFOOrdered si occupa di mantenere l'ordine delle richieste.
-func (kvs *KeyValueStoreSequential) receiveFIFOOrdered(args Common.Args) bool {
-	for {
-		kvs.receiveAssumeFIFOMutex.Lock()
-		if kvs.receiveAssumeFIFO == args.TimestampClient && args.Key != Common.EndKey {
-			kvs.receiveAssumeFIFO++
-			kvs.printDebugBlueArgs("RICEVUTO da client", args)
-			kvs.receiveAssumeFIFOMutex.Unlock()
-			return true
-		} else if args.Key == Common.EndKey {
-			kvs.printDebugBlueArgs("RICEVUTO da client", args)
-			kvs.receiveAssumeFIFOMutex.Unlock()
-			return true
-		}
-		kvs.receiveAssumeFIFOMutex.Unlock()
+func (kvs *KeyValueStoreSequential) SetRequestClient(id string, ts int) {
+	val, _ := kvs.GetClientMap(id)
+	val.SetRequestTs(ts)
+}
+
+func (kvs *KeyValueStoreSequential) IncreaseRequestTsClient(args common.Args) {
+	kvs.SetRequestClient(args.GetIDClient(), args.GetTimestamp()+1)
+}
+
+// TODO: errore qui, -1 gestire
+func (kvs *KeyValueStoreSequential) GetRequestTsClient(id string) (int, error) {
+	if clientMap, ok := kvs.GetClientMap(id); ok {
+		return clientMap.GetRequestTs(), nil
 	}
-
+	// Gestisci l'errore qui. Potresti restituire un valore predefinito o generare un errore.
+	return -1, fmt.Errorf("key non presente")
 }
 
-func (kvs *KeyValueStoreSequential) sendFIFOOrderedToClient(msg MessageS) bool {
-	for {
-		kvs.sendAssumeFIFOMutex.Lock()
-		if kvs.sendAssumeFIFO == msg.Args.TimestampClient && msg.Args.Key != Common.EndKey {
-			kvs.sendAssumeFIFO++
-			kvs.printGreen("ESEGUITO", msg)
-			kvs.sendAssumeFIFOMutex.Unlock()
-			return true
-		} else if msg.Args.Key == Common.EndKey {
-			kvs.sendAssumeFIFOMutex.Unlock()
-			return true
-		}
-		kvs.sendAssumeFIFOMutex.Unlock()
+// TODO : non usata
+func (kvs *KeyValueStoreSequential) IncreaseExecuteTsClient(args common.Args) {
+	clientMap := kvs.ClientMaps[args.GetIDClient()]
+	clientMap.MutexExecute.Lock()
+
+	executeTs := args.GetTimestamp() + 1
+	clientMap.SetExecuteTs(executeTs)
+
+	clientMap.MutexExecute.Unlock()
+	kvs.ClientMaps[args.GetIDClient()] = clientMap
+}
+
+func (kvs *KeyValueStoreSequential) GetExecuteTsClient(id string) (int, error) {
+	if clientMap, ok := kvs.ClientMaps[id]; ok {
+		return clientMap.GetExecuteTs(), nil
 	}
+	// Gestisci l'errore qui. Potresti restituire un valore predefinito o generare un errore.
+	return -1, fmt.Errorf("key non presente")
 }
-*/
