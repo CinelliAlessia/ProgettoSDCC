7617e1517324a2dd53dd75faffff5e566cb071f2
Alessia Cinelli <cinellialessia@gmail.com>
2024-05-03 16:32:23 +0200

    Separate funzioni per connessione dal file client.go
    Creata struttura dati per assunzione sul FIFO Ordering

--- main/client/client.go
+++ main/client/client.go
@@ -1,13 +1,27 @@
-// client.go
 package main
 
 import (
 	"fmt"
 	"main/common"
-	"math/rand"
-	"net/rpc"
+	_ "main/common"
+	sync2 "sync"
 	"time"
 )
+
+type ClientState struct {
+	SendIndex    []int
+	MutexSent    []sync2.Mutex
+	ReceiveIndex []int
+	MutexReceive []sync2.Mutex
+}
+
+// Inizializza lo stato del client
+var clientState = ClientState{
+	SendIndex:    make([]int, common.Replicas),
+	MutexSent:    make([]sync2.Mutex, common.Replicas),
+	ReceiveIndex: make([]int, common.Replicas),
+	MutexReceive: make([]sync2.Mutex, common.Replicas),
+}
 
 func main() {
 
@@ -47,20 +61,11 @@ func sequential(rpcName string) {
 	for {
 		// Stampa il menu interattivo
 		fmt.Println("Consistenza sequenziale, scegli il test da eseguire: ")
-		fmt.Println("1. Basic Test")
-		fmt.Println("2. Medium Test")
-		fmt.Println("3. Complex Test")
-		fmt.Println("4. Esci")
 
-		// Leggi l'input dell'utente per l'operazione
-		fmt.Print("Inserisci il numero dell'operazione desiderata: ")
-		var choice int
-		_, err := fmt.Scan(&choice)
-		if err != nil {
-			fmt.Println("Errore durante la lettura dell'input:", err)
+		choice, done := chooseFuncTest()
+		if done {
 			return
 		}
-		fmt.Println()
 
 		switch choice {
 		case 1:
@@ -84,20 +89,10 @@ func sequential(rpcName string) {
 func causal(rpcName string) {
 	for { // Stampa il menu interattivo
 		fmt.Println("Consistenza causale, scegli il test da eseguire: ")
-		fmt.Println("1. Basic Test")
-		fmt.Println("2. Medium Test")
-		fmt.Println("3. Complex Test")
-		fmt.Println("4. Esci")
-
-		// Leggi l'input dell'utente per l'operazione
-		fmt.Print("Inserisci il numero dell'operazione desiderata: ")
-		var choice int
-		_, err := fmt.Scan(&choice)
-		if err != nil {
-			fmt.Println("Errore durante la lettura dell'input:", err)
+		choice, done := chooseFuncTest()
+		if done {
 			return
 		}
-		fmt.Println()
 
 		switch choice {
 		case 1:
@@ -118,169 +113,20 @@ func causal(rpcName string) {
 	}
 }
 
-/* ----- FUNZIONI UTILIZZATE PER LA CONNESSIONE -----*/
+func chooseFuncTest() (int, bool) {
+	fmt.Println("1. Basic Test")
+	fmt.Println("2. Medium Test")
+	fmt.Println("3. Complex Test")
+	fmt.Println("4. Esci")
 
-// randomConnect restituisce una connessione random con un server definito in common/config.go
-func randomConnect() *rpc.Client {
-	// Genera un numero casuale tra 0 e il numero di repliche - 1
-	randomIndex := rand.Intn(common.Replicas)
-
-	// Ottengo l'indirizzo a cui connettermi
-	serverName := common.GetServerName(common.ReplicaPorts[randomIndex], randomIndex)
-
-	//fmt.Println("CLIENT: Contatto il server", serverName)
-	conn, err := rpc.Dial("tcp", serverName)
-
+	// Leggi l'input dell'utente per l'operazione
+	fmt.Print("Inserisci il numero dell'operazione desiderata: ")
+	var choice int
+	_, err := fmt.Scan(&choice)
 	if err != nil {
-		fmt.Println("randomConnect: Errore durante la connessione al server:", err)
-		return nil
+		fmt.Println("Errore durante la lettura dell'input:", err)
+		return 0, true
 	}
-	return conn
-}
-
-// specificConnect restituisce una connessione specifica con un server definito in common/config.go, tramite index passato in argomento
-// Attenzione: Indicizzazione parte da 0!
-func specificConnect(index int) *rpc.Client {
-	if index > common.Replicas {
-		_ = fmt.Errorf("index out of range")
-		return nil
-	}
-
-	// Ottengo l'indirizzo a cui connettermi
-	serverName := common.GetServerName(common.ReplicaPorts[index], index)
-
-	//fmt.Println("CLIENT: Contatto il server:", serverName)
-	conn, err := rpc.Dial("tcp", serverName)
-
-	if err != nil {
-		fmt.Println("specificConnect: Errore durante la connessione al server:", err)
-		return nil
-	}
-	return conn
-}
-
-// executeCall esegue un comando ad un server specifico. Il comando da eseguire viene specificato tramite i parametri inseriti
-// si occupa di eseguire le operazioni di put, get e del, in maniera sync o async e incrementa il timestamp dello specifico client
-func executeCall(index int, rpcName string, args common.Args, opType string, specOrRandom string) (common.Response, error) {
-	response := common.Response{}
-	var err error
-
-	switch specOrRandom {
-	case specific:
-		err = executeSpecificCall(index, rpcName, args, &response, opType)
-	case random:
-		err = executeRandomCall(rpcName, args, &response, opType)
-	}
-
-	if err != nil {
-		return response, err
-	}
-
-	return response, nil
-}
-
-// executeRandomCall:
-//   - rpcName rappresenta il nome della funzione RPC da chiamare + il tipo di operazione (put, get, delete)
-func executeRandomCall(rpcName string, args common.Args, response *common.Response, opType string) error {
-	conn := randomConnect()
-	if conn == nil {
-		return fmt.Errorf("executeRandomCall: Errore durante la connessione con un server random\n")
-	}
-
-	switch opType {
-	case sync:
-		// Esegui l'operazione in modo sincrono
-		err := syncCall(conn, args, response, rpcName)
-		if err != nil {
-			return err
-		}
-	case async:
-		// Esegui l'operazione in modo asincrono
-		err := asyncCall(conn, args, response, rpcName)
-		if err != nil {
-			return err
-		}
-	default:
-		// Gestisci i casi non previsti
-	}
-
-	return nil
-}
-
-// executeSpecificCall:
-//   - index rappresenta l'indice relativo al server da contattare, da 0 a (common.Replicas-1)
-//   - rpcName rappresenta il nome della funzione RPC da chiamare + il tipo di operazione (put, get, delete)
-func executeSpecificCall(index int, rpcName string, args common.Args, response *common.Response, opType string) error {
-
-	conn := specificConnect(index)
-	if conn == nil {
-		return fmt.Errorf("executeSpecificCall: Errore durante la connessione con il server specifico %d\n", index)
-	}
-
-	switch opType {
-	case sync:
-		// Esegui l'operazione in modo sincrono
-		err := syncCall(conn, args, response, rpcName)
-		if err != nil {
-			return err
-		}
-	case async:
-		// Esegui l'operazione in modo asincrono
-		err := asyncCall(conn, args, response, rpcName)
-		if err != nil {
-			return err
-		}
-	default:
-		// Gestisci i casi non previsti
-	}
-	return nil
-}
-
-// syncCall esegue una chiamata RPC ritardata utilizzando il client RPC fornito.
-// Prima di effettuare la chiamata, applica un ritardo casuale per simulare condizioni reali di rete.
-func syncCall(conn *rpc.Client, args common.Args, response *common.Response, rpcName string) error {
-
-	debugPrintRun(rpcName, args)
-
-	// Effettua la chiamata RPC
-	err := conn.Call(rpcName, args, response)
-	if err != nil {
-		return fmt.Errorf("syncCall: errore durante la chiamata RPC in client.call: %s\n", err)
-	}
-
-	debugPrintResponse(rpcName, args, *response)
-
-	err = conn.Close()
-	if err != nil {
-		return fmt.Errorf("syncCall: errore durante la chiusura della connessione in client.call: %s\n", err)
-	}
-	return nil
-}
-
-// asyncCall: utilizzato per lsa consistenza sequenziale
-func asyncCall(conn *rpc.Client, args common.Args, response *common.Response, rpcName string) error {
-
-	// Effettua la chiamata RPC
-	call := conn.Go(rpcName, args, response, nil)
-	debugPrintRun(rpcName, args)
-
-	go func() {
-		<-call.Done // Aspetta che la chiamata RPC sia completata
-		debugPrintResponse(rpcName, args, *response)
-
-		if call.Error != nil {
-			fmt.Printf("asyncCall: errore durante la chiamata RPC in client: %s\n", call.Error)
-			response.Done <- false
-		} else {
-			//debugPrintResponse(rpcName, args, *response)
-			response.Done <- true
-		}
-
-		err := conn.Close()
-		if err != nil {
-			fmt.Printf("asyncCall: errore durante la chiusura della connessione in client: %s\n", err)
-		}
-	}()
-
-	return nil
+	fmt.Println()
+	return choice, false
 }
---
+++ main/client/connection.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"fmt"
+	"main/common"
+	"math/rand"
+	"net/rpc"
+)
+
+/* ----- FUNZIONI UTILIZZATE PER LA CONNESSIONE -----*/
+
+// executeCall esegue un comando ad un server. Il comando da eseguire viene specificato tramite i parametri inseriti
+// si occupa di eseguire le operazioni di put, get e del, in maniera sync o async
+func executeCall(index int, rpcName string, args common.Args, opType string, specOrRandom string) (common.Response, error) {
+	response := common.Response{}
+	var err error
+
+	switch specOrRandom {
+	case specific:
+		err = executeSpecificCall(index, rpcName, args, &response, opType)
+	case random:
+		err = executeRandomCall(rpcName, args, &response, opType)
+	}
+
+	if err != nil {
+		return response, err
+	}
+
+	return response, nil
+}
+
+// executeRandomCall:
+//   - rpcName rappresenta il nome della funzione RPC da chiamare + il tipo di operazione (put, get, delete)
+func executeRandomCall(rpcName string, args common.Args, response *common.Response, opType string) error {
+	randomIndex := rand.Intn(common.Replicas)
+
+	conn, err := connect(randomIndex)
+	if conn == nil || err != nil {
+		return fmt.Errorf("executeRandomCall: Errore durante la connessione con il server specifico %d, %v\n",
+			randomIndex, err)
+	}
+
+	switch opType {
+	case sync:
+		// Esegui l'operazione in modo sincrono
+		err := syncCall(conn, randomIndex, args, response, rpcName)
+		if err != nil {
+			return err
+		}
+	case async:
+		// Esegui l'operazione in modo asincrono
+		err := asyncCall(conn, randomIndex, args, response, rpcName)
+		if err != nil {
+			return err
+		}
+	default:
+		// Gestisci i casi non previsti
+	}
+
+	return nil
+}
+
+// executeSpecificCall:
+//   - index rappresenta l'indice relativo al server da contattare, da 0 a (common.Replicas-1)
+//   - rpcName rappresenta il nome della funzione RPC da chiamare + il tipo di operazione (put, get, delete)
+func executeSpecificCall(index int, rpcName string, args common.Args, response *common.Response, opType string) error {
+
+	conn, err := connect(index)
+	if conn == nil || err != nil {
+		return fmt.Errorf("executeSpecificCall: Errore durante la connessione con il server specifico %d, %v\n",
+			index, err)
+	}
+
+	switch opType {
+	case sync:
+		// Esegui l'operazione in modo sincrono
+		err := syncCall(conn, index, args, response, rpcName)
+		if err != nil {
+			return err
+		}
+	case async:
+		// Esegui l'operazione in modo asincrono
+		err := asyncCall(conn, index, args, response, rpcName)
+		if err != nil {
+			return err
+		}
+	default:
+		// Gestisci i casi non previsti
+	}
+	return nil
+}
+
+func connect(index int) (*rpc.Client, error) {
+	if index > common.Replicas {
+		return nil, fmt.Errorf("connect: index out of range")
+	}
+
+	// Ottengo l'indirizzo a cui connettermi
+	serverName := common.GetServerName(common.ReplicaPorts[index], index)
+
+	//fmt.Println("CLIENT: Contatto il server:", serverName)
+	conn, err := rpc.Dial("tcp", serverName)
+	if err != nil {
+		fmt.Println("specificConnect: Errore durante la connessione al server:", err)
+		return nil, fmt.Errorf("specificConnect: Errore durante la connessione al server", err)
+	}
+	return conn, err
+}
+
+// syncCall esegue una chiamata RPC ritardata utilizzando il client RPC fornito.
+// Prima di effettuare la chiamata, applica un ritardo casuale per simulare condizioni reali di rete.
+func syncCall(conn *rpc.Client, index int, args common.Args, response *common.Response, rpcName string) error {
+
+	for {
+		if clientState.SendIndex[index] == args.GetTimestamp() {
+			clientState.MutexSent[index].Lock()
+			clientState.SendIndex[index]++
+			break
+		}
+	}
+
+	// Effettua la chiamata RPC
+	debugPrintRun(rpcName, args)
+	err := conn.Call(rpcName, args, response)
+	if err != nil {
+		return fmt.Errorf("syncCall: errore durante la chiamata RPC in client.call: %s\n", err)
+	}
+
+	clientState.MutexSent[index].Unlock()
+
+	waitToAccept(index, args, rpcName, response)
+
+	err = conn.Close()
+	if err != nil {
+		return fmt.Errorf("syncCall: errore durante la chiusura della connessione in client.call: %s\n", err)
+	}
+	return nil
+}
+
+// asyncCall: utilizzato per lsa consistenza sequenziale
+func asyncCall(conn *rpc.Client, index int, args common.Args, response *common.Response, rpcName string) error {
+
+	// TODO: per quanto riguarda la async a me non interessa inviarli in ordine
+	for {
+		if clientState.SendIndex[index] == args.GetTimestamp() {
+			clientState.MutexSent[index].Lock()
+			clientState.SendIndex[index]++
+			break
+		}
+	}
+
+	// Effettua la chiamata RPC
+	call := conn.Go(rpcName, args, response, nil)
+	debugPrintRun(rpcName, args)
+
+	clientState.MutexSent[index].Unlock()
+
+	go func() {
+		<-call.Done // Aspetta che la chiamata RPC sia completata
+		//TODO: le ricevi bene da parte del client, prendi la risposta in maniera ordinata !!!
+
+		waitToAccept(index, args, rpcName, response)
+
+		if call.Error != nil {
+			fmt.Printf("asyncCall: errore durante la chiamata RPC in client: %s\n", call.Error)
+			response.Done <- false
+		} else {
+			//debugPrintResponse(rpcName, args, *response)
+			response.Done <- true
+		}
+
+		err := conn.Close()
+		if err != nil {
+			fmt.Printf("asyncCall: errore durante la chiusura della connessione in client: %s\n", err)
+		}
+	}()
+
+	return nil
+}
+
+func waitToAccept(index int, args common.Args, rpcName string, response *common.Response) {
+	for {
+		if clientState.ReceiveIndex[index] == args.GetTimestamp() {
+			clientState.MutexReceive[index].Lock()
+			clientState.ReceiveIndex[index]++
+			debugPrintResponse(rpcName, args, *response)
+			clientState.MutexReceive[index].Unlock()
+			break
+		}
+	}
+}
